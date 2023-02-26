14c987d05fb66ce22135a2c2b053ed8483021d65
Or Rodrigez <orrodrigez1@gmail.com>
2023-02-26 11:30:03 +0200

    New projects will be pushed into the project list

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import './styles/styles.css';
 import './styles/header.css';
 import './styles/main.css';
 import './styles/sidebar.css';
-import switchForm from './scripts/addProject';
+import { switchForm, pushProj, refreshList } from './scripts/addProject';
 
-const newProject = document.getElementById('new-project');
-const submitProject = document.getElementById('submit-project');
-const newProjectInput = document.getElementById('new-project-input');
+// refresh project list on page load
+refreshList();
+
+
+const newProject = document.getElementById('new-project'); // add new button
+const submitProject = document.getElementById('submit-project'); // the + button to add project
+const newProjectInput = document.getElementById('new-project-input'); // the input for project name
 newProject.addEventListener('click', switchForm);
 submitProject.addEventListener('click', (e) => {
-  e.preventDefault()
+  e.preventDefault();
+  pushProj();
   newProjectInput.value = "";
-  switchForm()
+  switchForm();
 });
--- src/scripts/addProject.js
+++ src/scripts/addProject.js
@@ -1,7 +1,12 @@
 const newProject = document.getElementById('new-project');
 const projectForm = document.getElementById('project-form');
+const projects = [
+  {
+    title: "My First Project"
+  }
+]
 
-export default function switchForm() {
+function switchForm() {
   if(projectForm.classList.contains('hidden')) {
     newProject.classList.add('hidden');
     projectForm.classList.remove('hidden');
@@ -10,3 +15,32 @@ export default function switchForm() {
     newProject.classList.remove('hidden');
   }
 }
+
+// when + is clicked, push the new project name to the object arrays.
+function pushProj() {
+  const newProjectInput = document.getElementById('new-project-input');
+  const project = {
+    title: newProjectInput.value
+  }
+  projects.push(project);
+  refreshList();
+}
+
+function refreshList() {
+  const projectList = document.getElementById('projects-list');
+  // refresh the array to display the new list of projects.
+  projectList.textContent = "";
+
+  // make the project list to dynamically take its content from array.
+  projects.forEach((project) => {
+    const projTitle = document.createElement('h2');
+    projTitle.textContent = project.title;
+    projectList.appendChild(projTitle);
+  })
+}
+
+export { switchForm, pushProj, refreshList }
+
+
+
+
