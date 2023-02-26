d377d81aae060c77b57bb359f825b0cd7709fa4f
Or Rodrigez <orrodrigez1@gmail.com>
2023-02-26 15:17:32 +0200

    Increase the delay time(see last commit)

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ newProject.addEventListener('click', () => {
 newProjectInput.addEventListener('focusout', () => {
   setTimeout(() => {
     closeAddProjectForm();
-  }, 100);
+  }, 200);
 });
 submitProject.addEventListener('click', (e) => {
   e.preventDefault();
@@ -46,7 +46,7 @@ headerTitle.addEventListener('click', () => {
 renameInput.addEventListener('focusout', () => {
   setTimeout(() => {
     closeRenameForm();
-  }, 100);
+  }, 200);
 });
 saveName.addEventListener('click', (e) => {
   e.preventDefault();
