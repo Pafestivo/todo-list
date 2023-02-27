64f726eb5980a02bbb77bbad1d08e0fbf17cea6f
Or Rodrigez <orrodrigez1@gmail.com>
2023-02-27 09:56:03 +0200

    Create a constructor to make todo items

---
+++ src/scripts/createTodo.js
@@ -0,0 +1,31 @@
+export default function Todo(name, description, priority, dueDate) {
+
+  const id = crypto.randomUUID;
+  let done = false;
+  return {
+    get id() {
+      return id;
+    },
+    get name() {
+      return name;
+    },
+    get description() {
+      return description;
+    },
+    get priority() {
+      return priority;
+    },
+    get dueDate() {
+      return dueDate;
+    },
+    get isDone() {
+      return done;
+    },
+    set priority(updatedValue) {
+      this.priority = updatedValue;
+    },
+    set toggleDone(value) {
+      this.done = value
+    },
+  }
+}
