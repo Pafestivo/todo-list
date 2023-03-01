64f7cd5e1b7c71e1a40db89e394a0a0239dfbf24
Or Rodrigez <orrodrigez1@gmail.com>
2023-03-01 19:51:12 +0200

    Fix mobile

--- src/index.js
+++ src/index.js
@@ -44,11 +44,7 @@ document.addEventListener('keydown', (e) => { // shortcut escape to close forms
 
 newProject.addEventListener('click', openAddProjectForm);
 
-renameInput.addEventListener('focusout', () => {
-  setTimeout(() => {
-    closeRenameForm();
-  }, 150);
-});
+
 submitProject.addEventListener('click', (e) => {
   e.preventDefault();
   closeAddProjectForm();
